a34950c8fbd8dcbc0d382bdad1ef86585e062536
Charlotte Qvist <charlotteqvist@Charlottes-MacBook-Pro-2.local>
2016-04-14 13:30:48 +0200

    Add bluetooth

--- Arduino/6dof(Arduino)[V10]/Processing_Example/Arduino/FreeSix_cube/FreeSix_cube.ino
+++ Arduino/6dof(Arduino)[V10]/Processing_Example/Arduino/FreeSix_cube/FreeSix_cube.ino
@@ -5,6 +5,7 @@
 #include <FreeSixIMU.h>
 #include <FIMU_ADXL345.h>
 #include <FIMU_ITG3200.h>
+#include <SoftwareSerial.h>
 
 #define DEBUG
 #ifdef DEBUG
@@ -18,11 +19,21 @@
 
 float q[4]; //hold q values
 
+//BLUETOOTH
+const int RX_PIN = 10;
+const int TX_PIN = 9;
+const int BLUETOOTH_BAUD_RATE = 9600;
+
+SoftwareSerial bluetooth(RX_PIN, TX_PIN);
+
 // Set the FreeIMU object
 FreeSixIMU my3IMU = FreeSixIMU();
 
 void setup() {
-  Serial.begin(115200);
+  Serial.begin(9600);
+  bluetooth.begin(BLUETOOTH_BAUD_RATE);
+   
+  //Serial.begin(115200);
   Wire.begin();
 
   delay(5);
@@ -32,6 +43,13 @@ void setup() {
 
 
 void loop() { 
+  if (bluetooth.available()) {
+    Serial.write(bluetooth.read());
+  }
+  if (Serial.available()) {
+    bluetooth.write(Serial.read());
+  }
+  
   my3IMU.getQ(q);
   serialPrintFloatArr(q, 4);
   Serial.println(""); //line break
